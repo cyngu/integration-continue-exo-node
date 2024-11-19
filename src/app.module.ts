33229288384edad29f736f24fa21038f5ce59f3b
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-11-19 13:23:28 +0100

    feat: add mongodb connection and users api with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:password@localhost:27017/admin'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
