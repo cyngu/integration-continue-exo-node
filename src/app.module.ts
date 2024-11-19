cce0d85702ba5130c24f1b8ff1e2d0bca08c0a11
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-11-19 14:26:08 +0100

    feat: create backend service in docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:password@localhost:27017/admin'),
+    MongooseModule.forRoot('mongodb://root:password@mongodb:27017/admin'),
     UsersModule,
   ],
   controllers: [AppController],
