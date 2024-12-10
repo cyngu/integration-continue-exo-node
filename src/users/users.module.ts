6f5ace711c730716aaddcd184f4d24ec1187434e
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-12-10 14:42:13 +0100

    feat: move backend from other repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User, UserSchema } from './users.schema';
+import { RolesModule } from '../roles/roles.module';
 
 @Module({
   imports: [
+    RolesModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  exports: [UsersService]
 })
 export class UsersModule {}
