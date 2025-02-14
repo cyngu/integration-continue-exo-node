e823b972391543851f0caa5e1393ff740c74fbd4
Jamil18474 <jamil.abdelhamid@ynov.com>
2025-02-14 14:12:38 +0100

    feat: implement user deletion

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import {
+  Headers,
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './users.schema';
-import { JwtGuard } from "../guards/jwt.guards";
+import { JwtGuard } from '../guards/jwt.guards';
 
 @Controller('users')
 export class UsersController {
@@ -16,5 +25,16 @@ export class UsersController {
   @Get()
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
+  }
+
+  @UseGuards(JwtGuard)
+  @Delete(':id')
+  async remove(
+    @Param('id') id: string,
+    @Headers('authorization') authHeader: string,
+  ): Promise<void> {
+    const token = authHeader.split(' ')[1]; // Récupérer le token Bearer
+    // Vous pouvez maintenant utiliser le token comme nécessaire
+    return this.usersService.remove(id, token);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,5 +1,7 @@
 import {
+  BadRequestException,
   ConflictException,
+  ForbiddenException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -8,12 +10,14 @@ import { Model, Types } from 'mongoose';
 import { User, UserDocument } from './users.schema';
 import { Role, RoleDocument } from '../roles/roles.schema';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>,
     @InjectModel(Role.name) private readonly roleModel: Model<RoleDocument>,
+    private jwtService: JwtService,
   ) {}
 
   async findOneByEmail(email: string): Promise<UserDocument | null> {
@@ -26,7 +30,31 @@ export class UsersService {
       throw new ConflictException("Email d'utilisateur déjà pris");
     }
 
-    let defaultRole = await this.roleModel.findOne({ name: 'employee' });
+    if (!this.isValidName(user.name)) {
+      throw new BadRequestException('Nom invalide');
+    }
+    if (!this.isValidName(user.firstname)) {
+      throw new BadRequestException('Prénom invalide');
+    }
+    if (!this.isValidPassword(user.password)) {
+      throw new BadRequestException(
+        'Le mot de passe doit contenir au moins 6 caractères.',
+      );
+    }
+    if (!this.isValidEmail(user.email)) {
+      throw new BadRequestException('Email invalide');
+    }
+    if (this.isUnderage(user.birthDate)) {
+      throw new BadRequestException("L'utilisateur doit avoir au moins 18 ans");
+    }
+
+    if (!this.isValidName(user.city)) {
+      throw new BadRequestException('Ville invalide');
+    }
+    if (!this.isValidZipcode(user.zipcode)) {
+      throw new BadRequestException('Code postal invalide');
+    }
+    const defaultRole = await this.roleModel.findOne({ name: 'employee' });
     if (!defaultRole) {
       throw new NotFoundException(
         'Le rôle "employee" par défaut est introuvable',
@@ -46,5 +74,58 @@ export class UsersService {
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
+  }
+
+  async remove(id: string, token: string): Promise<void> {
+    // const usertodelete = await this.findById(id);
+    const decodedToken = await this.jwtService.decode(token);
+    if (decodedToken.role.name !== 'admin') {
+      throw new ForbiddenException(
+        'Vous devez être un administrateur pour supprimer cet utilisateur.',
+      );
+    }
+
+    if (!decodedToken.role.permissions.includes('delete')) {
+      throw new ForbiddenException(
+        "Vous n'avez pas la permission de supprimer cet utilisateur.",
+      );
+    }
+
+    await this.userModel.deleteOne({ _id: id });
+  }
+
+  // Méthodes de validation privées
+  private isUnderage(birthDate: Date): boolean {
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+
+    const monthDiff = today.getMonth() - birth.getMonth();
+    const dayDiff = today.getDate() - birth.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      age--;
+    }
+
+    return age < 18;
+  }
+
+  private isValidPassword(password: string): boolean {
+    return password && password.length >= 6;
+  }
+
+  private isValidEmail(email: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+
+  private isValidName(name: string): boolean {
+    const namePattern = /^[A-Za-zÀ-ÿ'-]+(?:\s[A-Za-zÀ-ÿ'-]+)*$/; // Allow letters, spaces, apostrophes, dashes
+    return namePattern.test(name);
+  }
+
+  private isValidZipcode(zipcode: string): boolean {
+    const zipcodePattern = /^[0-9]{5}$/;
+    return zipcodePattern.test(zipcode);
   }
 }
