487c6304e94769168ac1843e6583b907b2d4817d
Ssonyan <ssonyan@live.fr>
2025-02-28 23:55:43 +0100

    feat: e2e testing

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtGuard } from '../guards/jwt.guards';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'your-secret-key-for-testing',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtGuard],
+  exports: [JwtGuard, JwtModule],
 })
 export class AuthModule {}
